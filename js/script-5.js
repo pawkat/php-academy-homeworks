5c24b34ee8c22140a08a719957726205fb947c2c
Pavel Pashko <p.p.pawkat@gmail.com>
2017-05-21 18:30:04 +0300

    added hw-5

---
+++ js/script-5.js
@@ -0,0 +1,15 @@
+var name;
+var admin;
+var name = 'Harry';
+var admin = name;
+console.log(admin);
+var f = 'Теорема Пифагора';
+var a = 3;
+var b = 4;
+var c;
+var c_square;
+var c_square = Math.pow(a,2) + Math.pow(b,2);
+console.log(c_square);
+var c = Math.sqrt(c_square);
+console.log(c);
+
